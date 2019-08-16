9517f771d56087dc47e80b1996b3d4b85fe2fc0f
Marharyta Skliarenko <marharyta.skliarenko@MediaKind.com>
2019-08-16 15:13:09 +0300

    React monsters

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { Router, Route, hashHistory } from 'react-router';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
+import reducer from './reducers';
+import About from './About';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+     <Router >
+      <Route path="/" component={App}/>
+      <Route path="/about" component={About}/>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// import { createStore } from 'redux';
+//
+// function playlist(state = [], action) {
+//   if (action.type === 'ADD_TRACK') {
+//     return [
+//       ...state,
+//       action.payload
+//     ];
+//   }
+//   return state;
+// }
+//
+// const store = createStore(playlist);
+//
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+//
+// store.subscribe(() => {
+//   list.innerHTML = '';
+//   trackInput.value = '';
+//   store.getState().forEach(track => {
+//     const li = document.createElement('li');
+//     li.textContent = track;
+//     list.appendChild(li);
+//   });
+// })
+//
+//
+// addTrackBtn.addEventListener('click', () => {
+//   const trackName = trackInput.value;
+//   store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+// });
